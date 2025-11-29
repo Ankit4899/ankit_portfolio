a15f20fb4fee7715322c3db1288f98b048f4a9e6
Ankit raj <ankitrajbarhiya51@gmail.com>
2025-11-29 21:55:23 +0530

    Navbar Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import LogoSection from './components/LogoSection'
+import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import ShowcaseSections from './sections/ShowcaseSections'
 const App = () => {
   return (
     <>
+    <Navbar />
     <Hero />
     <ShowcaseSections />
+    <LogoSection />
     </>
   )
 }
