a48f10010f02329bc0686521eeb3749504425c3b
Ankit raj <ankitrajbarhiya51@gmail.com>
2026-01-05 11:58:28 +0530

    working experience section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import LogoSection from './components/LogoSection'
+import LogoSection from './sections/LogoSection'
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import ShowcaseSections from './sections/ShowcaseSections'
+import FeatureCards from './sections/FeatureCards'
+import ExperienceSection from './sections/ExperienceSection'
 const App = () => {
   return (
     <>
@@ -10,6 +12,8 @@ const App = () => {
     <Hero />
     <ShowcaseSections />
     <LogoSection />
+    <FeatureCards />
+    <ExperienceSection />
     </>
   )
 }
